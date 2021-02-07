dea6d62319ddd80e1fa3e89904fec5dce33f1526
Will Anderson <wmaxand@gmail.com>
2021-02-06 22:07:07 -0500

    Added mobile styles

---
+++ web/app.js
@@ -0,0 +1,12 @@
+var target = document.querySelector('#toggle');
+var menu = document.querySelector('body');
+
+target.onclick = function() {
+  menu.classList.toggle('show');
+  var x = document.getElementById("toggle");
+  if (x.innerHTML === "Menu") {
+      x.innerHTML = "Close";
+    } else {
+      x.innerHTML = "Menu";
+    }
+}
